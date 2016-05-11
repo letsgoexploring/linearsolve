4cdc3ac952643b527a582cb2a16589c83c1bc4e3
Brian Jenkins <letsgoexploring@gmail.com>
2016-05-10 21:59:00 -0700

    updated package

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'linearsolve',
   packages = ['linearsolve'],
-  version = '2.0.0a',
+  version = '2.0.1a',
   description = 'A package for solving dynamic stochastic general equilibrium (DSGE) models',
   author = 'Brian C. Jenkins',
   author_email = 'bcjenkin@uci.edu',
   url = 'https://github.com/letsgoexploring/linearsolve-package',
-  download_url = 'https://github.com/letsgoexploring/linearsolve-package/tarball/dist/linearsolve-2.0.0a.tar.gz',
+  # download_url = 'https://github.com/letsgoexploring/linearsolve-package/tarball/dist/linearsolve-2.0.0a.tar.gz',
   keywords = ['economics','solution','dsge','schur','macroconomics','rbc','new keynesian','business cycles'],
   classifiers = [],
 )
