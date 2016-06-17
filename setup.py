b5fd6b097fffdc9badcfd221213fb94756e89aba
Brian Jenkins <letsgoexploring@gmail.com>
2016-06-17 11:44:54 -0700

    updated package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'linearsolve',
   packages = ['linearsolve'],
-  version = '2.0.3',
+  version = '3.0.0',
   description = 'A package for solving dynamic stochastic general equilibrium (DSGE) models',
   author = 'Brian C. Jenkins',
   author_email = 'bcjenkin@uci.edu',
